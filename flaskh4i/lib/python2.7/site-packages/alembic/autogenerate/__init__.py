70ed09e3a1c78aeba6e9c56434f834255b901b19
Alex Piatski <piatskia@hntvlan569.0486.wlan.wireless-resnet.upenn.edu>
2015-10-12 16:22:45 -0400

    added basic marker functionality

--- flaskh4i/lib/python2.7/site-packages/alembic/autogenerate/__init__.py
+++
@@ -1,2 +0,0 @@
-from .api import compare_metadata, _produce_migration_diffs, \
-    _produce_net_changes
--- flaskh4i/lib/python2.7/site-packages/alembic/op.py
+++
@@ -1,6 +0,0 @@
-from .operations import Operations
-from . import util
-
-# create proxy functions for
-# each method on the Operations class.
-util.create_module_class_proxy(Operations, globals(), locals())

07b9c8491325182de0ea0b6b187196ea042d18bb
Thomas Lee <thomaslee1911@gmail.com>
2015-10-17 12:30:01 -0400

    incorporate adding to db and geocoding, added geocoder to requirements

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,5 @@
 from flask import url_for
+from app.models import IdlingIncident
 
 
 def register_template_utils(app):
@@ -21,7 +22,26 @@ def index_for_role(role):
 
 
 def parse_to_db(db, filename):
-    import csv
+    import csv, geocoder
+    city_default = ', philadelphia, pennsylvania, usa'
+    vehicle_id_index = 7;
+    location_index = 3;
+    date_index = 0;
+    agency_index = 6;
+    picture_index = 13;
+    description_index = 11;
+    
     with open(filename, 'rb') as file:
         reader = csv.reader(file, delimiter=',')
-        return reader.next()
+        columns = reader.next()
+        for row in reader:
+            print row
+            a = address=row[location_index]
+            g = geocoder.arcgis(a + city_default).latlng
+            l = Location(lat=g[0], long=g[1], address=a)
+            i = IdlingIncident(vehicle_id=row[vehicle_id_index], l,
+                date=row[date_index], agency=row[agency_index], picture=row[picture_index],
+                description=row[description_index])
+            db.session.add(i)
+            db.session.commit()
+        return columns

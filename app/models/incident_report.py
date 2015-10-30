a23e95e9dbfd2ae3095c4bdc7b78dea29a9530fd
Ben Sandler <ben1sandler@gmail.com>
2015-10-30 15:44:30 -0400

    Added fake lat and long

--- app/models/incident_report.py
+++ app/models/incident_report.py
@@ -44,7 +44,13 @@ class IncidentReport(db.Model):
 
         seed()
         for i in range(count):
-            l = Location(original_user_text=fake.address())
+            l = Location(
+                original_user_text=fake.address(),
+                latitude=str(fake.geo_coordinate(center=39.951021,
+                                                 radius=0.001)),
+                longitude=str(fake.geo_coordinate(center=-75.197243,
+                                                  radius=0.001))
+            )
             r = IncidentReport(
                 vehicle_id=fake.password(length=6, lower_case=False),
                 # Either sets license plate to '' or random 6 character string

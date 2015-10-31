f902f1ea6cd5889828ef8701c0e370bbafcdba8a
sanjayss34 <sanjay_ssubramanian@hotmail.com>
2015-10-30 20:37:21 -0400

    Updated basic map to be full screen with geolocation and markers for incident reports and time slider ui element

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from . import main
-
+from app import models, db
 
 @main.route('/')
 def index():
@@ -9,4 +9,30 @@ def index():
 
 @main.route('/map')
 def get_map():
-    return render_template('main/map.html')
+    #Get information on all Incident Reports in the db, and pass to
+    #map.html
+    vehicle_ids = []
+    license_plates = []
+    latitudes = []
+    longitudes = []
+    dates = []
+    durations = []
+    agencies = []
+    pictures = []
+    descriptions = []
+
+    for ir in models.IncidentReport.query.all():
+        vehicle_ids.append(str(ir.vehicle_id))
+        license_plates.append(str(ir.license_plate))
+
+#Pass latitudes, longitudes as floats so they can be used to fix
+        #locations of map markers
+        latitudes.append(float((ir.location).latitude))
+        longitudes.append(float((ir.location).longitude))
+        dates.append(str(ir.date))
+        durations.append(str(ir.duration))
+        agencies.append(str(ir.agency.name))
+        pictures.append(ir.picture_url)
+        descriptions.append(str(ir.description))
+
+    return render_template('main/map.html', vehicle_ids=vehicle_ids, license_plates=license_plates, latitudes=latitudes, longitudes=longitudes, dates=dates, durations=durations, agencies=agencies, pictures=pictures, descriptions=descriptions)
--- app/static/js/initialize_map.js
+++ app/static/js/initialize_map.js
@@ -1,9 +1,81 @@
 geocoder = new google.maps.Geocoder();
 address = document.getElementById("address");
+//Initial map center coordinates
 INITIAL_CENTER_LAT = 39.952;
 INITIAL_CENTER_LONG = -75.195;
-ZOOM = 17;
 initial_coords = new google.maps.LatLng(INITIAL_CENTER_LAT, INITIAL_CENTER_LONG);
+//Initial map zoom value
+ZOOM = 17;
+//An integer for the offset between the actual window size and the
+//JavaScript-determined Window size in order to size the map
+WINDOW_OFFSET = 70
+
+//Bounds for the Date Slider. End date is current date.
+var BOUNDS_MIN = new Date(2015, 0, 1);
+var BOUNDS_MAX = new Date();
+
+//Get Incident Report information through HTML and Jinja2 and add markers
+//to the map.
+function addMarkers(map) {
+    var vehicle_ids_str = $("#vehicle_ids").data();
+    var license_plates_str = $("#license_plates").data();
+    var latitudes = $("#latitudes").data();
+    var longitudes = $("#longitudes").data();
+    var dates_str = $("#dates").data();
+    var durations_str = $("#durations").data();
+    var agencies_str = $("#agencies").data();
+    var pictures_str = $("#pictures").data();
+    var descriptions_str = $("#descriptions").data();
+    //Tokenize the strings representing arrays that are given through HTML
+    //by Jinja2
+    var vehicle_ids = (vehicle_ids_str.name).split('\'');
+    var license_plates = (license_plates_str.name).split('\'');
+    var dates = (dates_str.name).split('\'');
+    var durations = (durations_str.name).split('\'');
+    var agencies = (agencies_str.name).split('\'');
+    var pictures = (pictures_str.name).split('\'');
+    var descriptions = (descriptions_str.name).split('\'');
+    for (i = 0; i < (latitudes.name).length; i = i + 1) {
+        //In order that the same marker isn't modified every time,
+        //we create a new function here to create and design each marker
+        $(function() {
+            var marker = new google.maps.Marker({
+                //Use latitude and longitude values from incident report
+                //to set position of marker
+                position:{lat: latitudes.name[i], lng: longitudes.name[i]}
+            });
+
+            //Tie marker to map passed as an argument
+            
+            marker.setMap(map);
+            
+            //Information presented when marker is clicked
+            var contentString = '<div id="content">' +
+                '<div id="siteNotice">' +
+                '</div>' +
+                '<h1 id="firstHeading" class="firstHeading">Vehicle ID: ' + vehicle_ids[2*i+1] +'</h1>' +
+                '<div id="bodyContent">' +
+                '<p>License Plate: ' + license_plates[2*i+1] + '</p>' +
+                '<p>Date: ' + dates[2*i+1] + '</p>' +
+                '<p>Duration: ' + durations[2*i+1] + '</p>' +
+                '<p>Agency: ' + agencies[2*i+1] + '</p>' +
+                '<p>Link to Picture: ' + pictures[2*i+1] + '</p>' +
+                '<p>Description: ' + descriptions[2*i+1] + '</p>' +
+                '</div>' +
+                '</div>';
+            var infoWindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+
+            //Add click listener to marker for displaying infoWindow
+            marker.addListener('click', function() {
+                infoWindow.open(map, marker);
+            });
+        });
+    }
+}
+
+//Initialize map and add markers
 function initialize() {
     var mapProp = {
         center: initial_coords,
@@ -11,6 +83,8 @@ function initialize() {
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+    //Use HTML geolocation to center map if possible
     if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function(position) {
                 var pos_center = {
@@ -24,15 +98,11 @@ function initialize() {
     else {
         console.log("Browser is not supporting geolocation.");
     }
-   /*var center_lat = geoplugin_latitude();
-    var center_lng = geoplugin_longitude();
-    var pos_center = {
-        lat: center_lat,
-        lng: center_lng
-    };
-    map.setCenter(pos_center);*/
+    addMarkers(map);
 }
 
+//Use Google geocoder to update geolocation given an address through
+//the address search box
 function update_center() {
     geocoder = new google.maps.Geocoder();
     address = $("#address").val();
@@ -53,15 +123,16 @@ function update_center() {
     }
 }
 
+//Get address submit event
 $(document).ready(function() {
     $('#addressForm').on('submit', function (event) {
         update_center();
         return false;
     });
 });
+
+//Date Slider - TODO
 $(function() {
-    var BOUNDS_MIN = new Date(2015, 0, 1);
-    var BOUNDS_MAX = new Date();
     $("#slider").dateRangeSlider({
         bounds: {
             min: BOUNDS_MIN,
@@ -74,4 +145,11 @@ $(function() {
         },
     });
 });
+
+//Add the map to the window
 google.maps.event.addDomListener(window, 'load', initialize)
+
+//Set the map's height to be (hopefully) full window height
+$(function() {
+    $("#googleMap").css("height", $(window).height()-WINDOW_OFFSET);
+});

32bf622f96734e593a0d6b24f3cda1060815e1ac
sanjayss34 <sanjay_ssubramanian@hotmail.com>
2015-11-12 23:40:44 -0500

    Cleaned up views.py

--- app/main/views.py
+++ app/main/views.py
@@ -13,38 +13,4 @@ def get_map():
     """Get information on all Incident Reports in the db, and
     pass to map.html
     """
-    '''vehicle_ids = []
-    license_plates = []
-    latitudes = []
-    longitudes = []
-    dates = []
-    durations = []
-    agencies = []
-    pictures = []
-    descriptions = []
-
-    for ir in IncidentReport.query.all():
-        vehicle_ids.append(str(ir.vehicle_id))
-        license_plates.append(str(ir.license_plate))
-
-        # Pass latitudes, longitudes as floats so they can be used to fix
-        # locations of map markers
-        latitudes.append(float((ir.location).latitude))
-        longitudes.append(float((ir.location).longitude))
-        dates.append(str(ir.date))
-        durations.append(str(ir.duration))
-        agencies.append(str(ir.agency.name))
-        pictures.append(ir.picture_url)
-        descriptions.append(str(ir.description))
-
-    return render_template('main/map.html', vehicle_ids=vehicle_ids,
-                           license_plates=license_plates,
-                           latitudes=latitudes,
-                           longitudes=longitudes,
-                           dates=dates,
-                           durations=durations,
-                           agencies=agencies,
-                           pictures=pictures,
-                           descriptions=descriptions)
-    '''
     return render_template('main/map.html', incident_reports=IncidentReport.query.all())

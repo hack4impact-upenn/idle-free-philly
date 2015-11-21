6ba9f9aa9764a46c6903d7f9a4564e266cabf283
Kyle Rosenbluth <kyle.rosenbluth@gmail.com>
2015-11-21 16:34:45 -0500

    add incident report to the cookie

--- app/main/messaging.py
+++ app/main/messaging.py
@@ -2,25 +2,26 @@ from flask import request, make_response
 from . import main
 from datetime import datetime, timedelta
 import twilio.twiml
-
-SECRET_KEY = '7c\xf9\r\xa7\xea\xdc\xef\x96\xf7\x8c\xaf\xdeW!\x81jp\xf7[}%\xda2'  # noqa
+import json
 
 
 @main.route("/report_incident", methods=['GET', 'POST'])
 def handle_message():
     message = str(request.values.get('Body'))  # noqa
     twiml = twilio.twiml.Response()
-    print twiml
-
     step = int(request.cookies.get('messagecount', 0))
-
+    incident_report = json.loads(request.cookies.get('incident_report', "{}"))
+    print(incident_report)
     if step is 0 and "report" in message.lower():
         twiml.message("Which Agency Owns the Vehicle? A)SEPTA Bus, B)SEPTA CCT, C)SEPTA, D)PWD, E)PECO, F)Streets, G)Others")  # noqa
     elif step is 1:
+        incident_report["agency"] = "SEPTOR"
         twiml.message("What is the License Plate Number? (eg.MG-1234E)")
     elif step is 2:
+        incident_report["license_plate"] = "TRAP"
         twiml.message("What is the Vehicle ID? (eg.105014)")
     elif step is 3:
+        incident_report["ID"] = "COOKOO FOR COCOAPUFFS"
         twiml.message("How many minutes has it been Idling for? (eg. 10)")
     elif step is 4:
         twiml.message("Please describe the situation (eg. The driver is sleeping)")  # noqa
@@ -32,4 +33,5 @@ def handle_message():
     expires_str = expires.strftime('%a, %d %b %Y %H:%M:%S GMT')
     response = make_response(str(twiml))
     response.set_cookie('messagecount', value=str(step), expires=expires_str)
+    response.set_cookie('incident_report', value=json.dumps(incident_report), expires=expires_str)  # noqa
     return response

0b91a5532f25e9b45ba5da3051d11a5931081e1c
Kyle Rosenbluth <kyle.rosenbluth@gmail.com>
2015-11-21 17:33:07 -0500

    Remove changes for demo

--- app/main/messaging.py
+++ app/main/messaging.py
@@ -2,8 +2,7 @@ from flask import request, make_response
 from . import main
 from datetime import datetime, timedelta
 import twilio.twiml
-import json
-import urllib
+# import json
 
 
 @main.route("/report_incident", methods=['GET', 'POST'])
@@ -11,18 +10,17 @@ def handle_message():
     message = str(request.values.get('Body'))  # noqa
     twiml = twilio.twiml.Response()
     step = int(request.cookies.get('messagecount', 0))
-    print urllib.unquote(request.cookies.get('report', "{}")).decode('ascii')  # noqa
-    incident_report = json.loads(request.cookies.get('report', "{}"))
+    # incident_report = json.loads(request.cookies.get('report', "{}"))
     if step is 0 and "report" in message.lower():
         twiml.message("Which Agency Owns the Vehicle? A)SEPTA Bus, B)SEPTA CCT, C)SEPTA, D)PWD, E)PECO, F)Streets, G)Others")  # noqa
     elif step is 1:
-        incident_report["agency"] = "SEPTOR"
+        # incident_report["agency"] = "SEPTOR"
         twiml.message("What is the License Plate Number? (eg.MG-1234E)")
     elif step is 2:
-        incident_report["licensePlate"] = "TRAP"
+        # incident_report["licensePlate"] = "TRAP"
         twiml.message("What is the Vehicle ID? (eg.105014)")
     elif step is 3:
-        incident_report["ID"] = "COOKOOFORCOCOAPUFFS"
+        # incident_report["ID"] = "COOKOOFORCOCOAPUFFS"
         twiml.message("How many minutes has it been Idling for? (eg. 10)")
     elif step is 4:
         twiml.message("Please describe the situation (eg. The driver is sleeping)")  # noqa
@@ -34,6 +32,5 @@ def handle_message():
     expires_str = expires.strftime('%a, %d %b %Y %H:%M:%S GMT')
     response = make_response(str(twiml))
     response.set_cookie('messagecount', value=str(step), expires=expires_str)
-    print(json.dumps(incident_report))
-    response.set_cookie('report', value=json.dumps(incident_report), expires=expires_str)  # noqa
+    # response.set_cookie('report', value=json.dumps(incident_report), expires=expires_str)  # noqa
     return response

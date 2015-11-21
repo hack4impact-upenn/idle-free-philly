fe6843c7832eb71f2ffaf4fc8411746091b89c24
Alex Piatski <piatskia@vagvlan531.0303.apn.wlan.wireless-pennnet.upenn.edu>
2015-11-21 17:23:46 -0500

    incorporated form validation and requirements. merged with sanjays branch. added date, time, and agency form capturing.

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,9 @@
 from flask.ext.wtf import Form
 from wtforms.fields import StringField, PasswordField, SubmitField, IntegerField, TextAreaField, HiddenField, \
-                                        BooleanField, FileField, FloatField
+                                        BooleanField, FileField, FloatField, DateField
+from wtforms import validators
+from ..models import Agency
 from werkzeug.datastructures import MultiDict
-
 from wtforms.fields.html5 import EmailField, TelField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, Optional
@@ -13,18 +14,32 @@ from ..custom_validators import (
 )
 from ..models import Role
 from .. import db
-
+import datetime as datetime
 class NewIncidentForm(Form):
-    license_plate_number = StringField('Vehicle ID')
-    vehicle_ID = StringField('License Plate Number')
-    notes = StringField('Notes')
+    license_plate_number = StringField('License Plate Number', [
+        validators.required('license plate is required'),
+        validators.Regexp('^\w+$', message ='license plate number must consist of only letters and numbers!'),
+        validators.Length(min = 6, max = 7)
+    ])
+    agency = QuerySelectField('Truck Agency ', validators=[DataRequired()], get_label='name',
+                              query_factory=lambda: db.session.query(Agency))
+    vehicle_ID = StringField('Vehicle ID', [
+        validators.required('vehicleID is required'),
+        validators.Regexp('^[0-9]*$', message='vehicle id must consist of only digits!'),
+        validators.Length(min = 2, max = 10)
+    ])
+    notes = StringField('Notes', [
+        validators.optional(),
+        validators.Length(max = 100)
+    ])
     latitude = FloatField('latitude')
+    date = DateField('date', default=datetime.date.today(), validators=[validators.DataRequired()])
     location = StringField('Location')
     longitude = FloatField('longitude')
-    current_longitude = HiddenField('current_longitude')
-    current_latitude = HiddenField('current_latitude')
-    use_current_location = BooleanField('Use Current Location')
-    idling_duration = IntegerField('Idling Duration')
+    idling_duration = IntegerField('Idling Duration (in minutes)', [
+        validators.required('idling duration (in minutes) is required!'),
+        validators.NumberRange(min = 0, max = 1000, message='invalid duration range!')
+    ])
     submit = SubmitField('Create Report')
 
 
--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,10 @@ from flask import render_template, request, flash
 from . import main
 from app import models, db
 from forms import NewIncidentForm
-from app.models import IncidentReport
+from app.models import IncidentReport, Agency
+from faker import Faker
+from datetime import timedelta
+from random import  choice
 
 
 @main.route('/')
@@ -11,42 +14,22 @@ def index():
 
 @main.route('/map', methods =['GET','POST'])
 def get_map():
-    #Get information on all Incident Reports in the db, and pass to
-    #map.html
-    vehicle_ids = []
-    license_plates = []
-    latitudes = []
-    longitudes = []
-    dates = []
-    durations = []
-    agencies = []
-    pictures = []
-    descriptions = []
-
-    """Create a new form."""
     form = NewIncidentForm()
-    current_loc = "testing123"
-    if form.validate_on_submit():
-        from datetime import timedelta
-        from faker import Faker
-        from random import seed, choice, randint
-        fake = Faker()
+    agencies = Agency.query.all()
 
-        current_address = ""
-        address = ""
+    if form.validate_on_submit():
         l = models.Location(original_user_text =form.location.data, latitude= form.latitude.data,
                             longitude= form.longitude.data)
-        agencies = models.Agency.query.all()
+
         new_incident = models.IncidentReport(
             vehicle_id=form.vehicle_ID.data,
             license_plate=form.license_plate_number.data,
             location=l,
-            date=fake.date_time_between(start_date="-1y", end_date="now"),
-            duration=timedelta(minutes=randint(1, 30)),
-            agency=choice(agencies),
-            picture_url=fake.image_url(),
+            date= form.date.data,
+            duration=timedelta(minutes= form.idling_duration.data),
+            agency= form.agency.data,
             description=form.notes.data,
         )
         db.session.add(new_incident)
         db.session.commit()
-    return render_template('main/map.html', form=form, incident_reports=IncidentReport.query.all())
+    return render_template('main/map.html', agencies=agencies, form=form, incident_reports=IncidentReport.query.all())

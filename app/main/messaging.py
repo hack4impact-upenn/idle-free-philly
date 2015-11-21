e3c98031dca1ebf3300e9d1500504defacbe0623
Kyle Rosenbluth <kyle.rosenbluth@gmail.com>
2015-11-21 17:11:29 -0500

    Temp

--- app/main/messaging.py
+++ app/main/messaging.py
@@ -10,14 +10,14 @@ def handle_message():
     message = str(request.values.get('Body'))  # noqa
     twiml = twilio.twiml.Response()
     step = int(request.cookies.get('messagecount', 0))
-    incident_report = json.loads(request.cookies.get('inc_report', "{}"))
+    incident_report = json.loads(request.cookies.get('i_report', "{}"))
     if step is 0 and "report" in message.lower():
         twiml.message("Which Agency Owns the Vehicle? A)SEPTA Bus, B)SEPTA CCT, C)SEPTA, D)PWD, E)PECO, F)Streets, G)Others")  # noqa
     elif step is 1:
         incident_report["agency"] = "SEPTOR"
         twiml.message("What is the License Plate Number? (eg.MG-1234E)")
     elif step is 2:
-        incident_report["license_plate"] = "TRAP"
+        incident_report["licensePlate"] = "TRAP"
         twiml.message("What is the Vehicle ID? (eg.105014)")
     elif step is 3:
         incident_report["ID"] = "COOKOO FOR COCOAPUFFS"
@@ -33,5 +33,5 @@ def handle_message():
     response = make_response(str(twiml))
     response.set_cookie('messagecount', value=str(step), expires=expires_str)
     print(json.dumps(incident_report))
-    response.set_cookie('inc_report', value=json.dumps(incident_report), expires=expires_str)  # noqa
+    response.set_cookie('i_report', value=json.dumps(incident_report), expires=expires_str)  # noqa
     return response

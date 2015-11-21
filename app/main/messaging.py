a6b259dce390ad2c03ddc015a381e5546444af9c
Kyle Rosenbluth <kyle.rosenbluth@gmail.com>
2015-11-21 17:08:40 -0500

    Temp

--- app/main/messaging.py
+++ app/main/messaging.py
@@ -10,7 +10,7 @@ def handle_message():
     message = str(request.values.get('Body'))  # noqa
     twiml = twilio.twiml.Response()
     step = int(request.cookies.get('messagecount', 0))
-    incident_report = json.loads(request.cookies.get('incident_report', "{}"))
+    incident_report = json.loads(request.cookies.get('inc_report', "{}"))
     if step is 0 and "report" in message.lower():
         twiml.message("Which Agency Owns the Vehicle? A)SEPTA Bus, B)SEPTA CCT, C)SEPTA, D)PWD, E)PECO, F)Streets, G)Others")  # noqa
     elif step is 1:
@@ -33,5 +33,5 @@ def handle_message():
     response = make_response(str(twiml))
     response.set_cookie('messagecount', value=str(step), expires=expires_str)
     print(json.dumps(incident_report))
-    response.set_cookie('incident_report', value=json.dumps(incident_report), expires=expires_str)  # noqa
+    response.set_cookie('inc_report', value=json.dumps(incident_report), expires=expires_str)  # noqa
     return response

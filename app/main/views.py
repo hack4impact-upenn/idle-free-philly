3f057fc7dbb9520ad0f2510bfa1b88c2bcb27c0a
Alex Piatski <piatskia@seas1491.wireless-pennnet.upenn.edu>
2015-12-02 12:15:28 -0500

    Fixed pep8 violations

--- app/main/views.py
+++ app/main/views.py
@@ -1,35 +1,38 @@
-from flask import render_template, request, flash
+from flask import render_template
 from . import main
 from app import models, db
 from forms import NewIncidentForm
 from app.models import IncidentReport, Agency
-from faker import Faker
 from datetime import timedelta
-from random import  choice
 
 
 @main.route('/')
 def index():
     return render_template('main/index.html')
 
-@main.route('/map', methods =['GET','POST'])
+
+@main.route('/map', methods=['GET', 'POST'])
 def get_map():
     form = NewIncidentForm()
     agencies = Agency.query.all()
 
     if form.validate_on_submit():
-        l = models.Location(original_user_text =form.location.data, latitude= form.latitude.data,
-                            longitude= form.longitude.data)
+        l = models.Location(original_user_text=form.location.data,
+                            latitude=form.latitude.data,
+                            longitude=form.longitude.data)
 
         new_incident = models.IncidentReport(
             vehicle_id=form.vehicle_ID.data,
             license_plate=form.license_plate_number.data,
             location=l,
-            date= form.date.data,
-            duration=timedelta(minutes= form.idling_duration.data),
-            agency= form.agency.data,
+            date=form.date.data,
+            duration=timedelta(minutes=form.idling_duration.data),
+            agency=form.agency.data,
             description=form.notes.data,
         )
         db.session.add(new_incident)
         db.session.commit()
-    return render_template('main/map.html', agencies=agencies, form=form, incident_reports=IncidentReport.query.all())
+    return render_template('main/map.html',
+                           agencies=agencies,
+                           form=form,
+                           incident_reports=IncidentReport.query.all())

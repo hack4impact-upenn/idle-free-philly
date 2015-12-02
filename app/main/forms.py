190c91ea95c0083bab7a204d673cf9887237bdf8
Alex Piatski <piatskia@seas1491.wireless-pennnet.upenn.edu>
2015-12-02 12:29:00 -0500

    fixing pep8 cont.

--- app/main/forms.py
+++ app/main/forms.py
@@ -12,14 +12,19 @@ import datetime as datetime
 class NewIncidentForm(Form):
     license_plate_number = StringField('License Plate Number', [
         validators.required('license plate is required'),
-        validators.Regexp('^\w+$', message='license plate number must consist of only letters and numbers!'),
+        validators.Regexp('^\w+$',
+                          message='license plate number must '
+                                  'consist of only letters and numbers!'),
         validators.Length(min=6, max=7)
     ])
-    agency = QuerySelectField('Truck Agency ', validators=[DataRequired()], get_label='name',
+    agency = QuerySelectField('Truck Agency ',
+                              validators=[DataRequired()],
+                              get_label='name',
                               query_factory=lambda: db.session.query(Agency))
     vehicle_ID = StringField('Vehicle ID', [
         validators.required('vehicleID is required'),
-        validators.Regexp('^[0-9]*$', message='vehicle id must consist of only digits!'),
+        validators.Regexp('^[0-9]*$',
+                          message='vehicle id must consist of only digits!'),
         validators.Length(min=2, max=10)
     ])
     notes = StringField('Notes', [
@@ -27,11 +32,14 @@ class NewIncidentForm(Form):
         validators.Length(max=100)
     ])
     latitude = FloatField('latitude')
-    date = DateField('date', default=datetime.date.today(), validators=[validators.DataRequired()])
+    date = DateField('date', default=datetime.date.today(),
+                     validators=[validators.DataRequired()])
     location = StringField('Location')
     longitude = FloatField('longitude')
     idling_duration = IntegerField('Idling Duration (in minutes)', [
         validators.required('idling duration (in minutes) is required!'),
-        validators.NumberRange(min=0, max=1000, message='invalid duration range!')
+        validators.NumberRange(min=0,
+                               max=1000,
+                               message='invalid duration range!')
     ])
     submit = SubmitField('Create Report')

75ee23771920285f325d030f838e841d63163556
Alex Piatski <piatskia@seas1491.wireless-pennnet.upenn.edu>
2015-12-02 12:17:23 -0500

    testing for pep8

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, flash
+from flask import render_template
 from . import main
 from app import models, db
 from forms import NewIncidentForm
@@ -11,22 +11,22 @@ def index():
     return render_template('main/index.html')
 
 
-@main.route('/map', methods =['GET','POST'])
+@main.route('/map', methods=['GET', 'POST'])
 def get_map():
     form = NewIncidentForm()
     agencies = Agency.query.all()
 
-    if form.validate_on_submit() :
-        l = models.Location(original_user_text =form.location.data, latitude=form.latitude.data,
-                            longitude= form.longitude.data)
+    if form.validate_on_submit():
+        l = models.Location(original_user_text=form.location.data, latitude=form.latitude.data,
+                            longitude=form.longitude.data)
 
         new_incident = models.IncidentReport(
             vehicle_id=form.vehicle_ID.data,
             license_plate=form.license_plate_number.data,
             location=l,
-            date= form.date.data,
-            duration=timedelta(minutes= form.idling_duration.data),
-            agency= form.agency.data,
+            date=form.date.data,
+            duration=timedelta(minutes=form.idling_duration.data),
+            agency=form.agency.data,
             description=form.notes.data,
         )
         db.session.add(new_incident)

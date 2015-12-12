bbdb1e49e4e2476add80ee1d130e47a087ed293f
Alex Piatski <piatskia@hntvlan569.1029.wlan.wireless-resnet.upenn.edu>
2015-12-11 21:03:24 -0500

    Continue merging Sanjay branc

--- app/main/views.py
+++ app/main/views.py
@@ -1,16 +1,12 @@
 from flask import render_template
 from ..models import EditableHTML, IncidentReport
 from . import main
-<<<<<<< HEAD
 from app import models, db
 from forms import NewIncidentForm
 from app.models import IncidentReport, Agency
 from datetime import timedelta
 
 
-@main.route('/')
-def index():
-    return render_template('main/index.html')
 
 
 @main.route('/map', methods=['GET', 'POST'])
@@ -35,19 +31,15 @@ def get_map():
         db.session.add(new_incident)
         db.session.commit()
     return render_template('main/map.html',
-                           agencies=agencies,
-                           form=form,
-=======
+                           incident_reports = IncidentReport.query.all())
 
 
-@main.route('/')
 @main.route('/map')
 def index():
     """Get information on all Incident Reports in the db, and
     pass to map.html
     """
     return render_template('main/map.html',
->>>>>>> c2c121c37b865a8aa4c108eccf9d4d473df17be2
                            incident_reports=IncidentReport.query.all())
 
 

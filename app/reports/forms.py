5d252d271b9af0dd8104189fc5404ca8fa59d332
Nancy Wong <nancypwong@gmail.com>
2015-12-12 15:09:42 -0500

    Move incident report forms to reports

--- app/reports/forms.py
+++ app/reports/forms.py
@@ -1,25 +1,77 @@
+import datetime as datetime
+
 from flask.ext.wtf import Form
-from wtforms.fields import StringField, SubmitField
-# from wtforms.fields.html5 import EmailField, TelField
-# from wtforms.ext.sqlalchemy.fields import QuerySelectField
-# from wtforms.validators import Length, Email, Optional, InputRequired
-# from ..custom_validators import (...)
-# TODO validate form
-# from ..models import Role
-# from .. import db
+from wtforms.fields import StringField, SubmitField, IntegerField, \
+    TextAreaField, HiddenField, DateField, FileField
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from wtforms.validators import (
+    InputRequired,
+    Regexp,
+    Length,
+    Optional,
+    NumberRange
+)
+
+from ..models import Agency
+from .. import db
 
 
-class ChangeReportInfoForm(Form):
-    # TODO set fields and validators
-    vehicle_id = StringField('Vehicle ID')
-    license_plate = StringField('License Plate', validators=[
+class IncidentReportForm(Form):
+    vehicle_id = StringField('Vehicle ID', validators=[
+        InputRequired('Vehicle ID is required.'),
+        Length(min=2, max=10,
+               message='Vehicle ID must be between 2 to 10 characters.'),
+        Regexp("^[a-zA-Z0-9]*$",
+               message='License plate number must '
+                       'consist of only letters and numbers.'),
     ])
-    location = StringField('Location')
-    date = StringField('Date')
-    duration = StringField('Duration')
-    # TODO dropdown of agencies + fill in the box for 'Other'
-    agency = StringField('Agency')
-    picture_url = StringField('Picture URL')
-    description = StringField('Description')
 
-    submit = SubmitField('Update report')
+    license_plate = StringField('License Plate Number', validators=[
+        Optional(),
+        Regexp("^[a-zA-Z0-9]*$",
+               message='License plate number must '
+                       'consist of only letters and numbers.'),
+        Length(min=6, max=7)
+    ])
+
+    bus_number = IntegerField('Bus Number', validators=[
+        Optional()
+    ])
+
+    led_screen_number = IntegerField('LED Screen Number', validators=[
+        Optional()
+    ])
+
+    latitude = HiddenField('Latitude')
+    longitude = HiddenField('Longitude')
+    location = StringField('Address')
+
+    date = DateField('Date', default=datetime.date.today(),
+                     validators=[InputRequired()])
+
+    duration = IntegerField('Idling Duration (hour:minutes:seconds)', [
+        InputRequired('Idling duration (hours:minutes:seconds) is required.'),
+        NumberRange(min=0,
+                    max=10000,
+                    message='Idling duration must be between'
+                            '0 and 10000 minutes.')
+    ])
+
+    agency = QuerySelectField('Vehicle Agency ',
+                              validators=[InputRequired()],
+                              get_label='name',
+                              query_factory=lambda: db.session.query(Agency))
+
+    picture = FileField('Upload a picture of the idling vehicle.',
+                        validators=[Optional()])
+
+    description = TextAreaField('Additional Notes', [
+        Optional(),
+        Length(max=5000)
+    ])
+
+    submit = SubmitField('Create Report')
+
+
+class EditIncidentReportForm(IncidentReportForm):
+    submit = SubmitField('Update Report')

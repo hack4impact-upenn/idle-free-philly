dc66503d4f21721345460614dabd85e868e88967
Kyle Rosenbluth <kyle.rosenbluth@gmail.com>
2015-12-12 13:30:33 -0500

    add location

--- app/main/messaging.py
+++ app/main/messaging.py
@@ -3,6 +3,7 @@ from . import main
 from .. import db
 from ..models import Agency
 from ..models import IncidentReport
+from ..models import Location
 from datetime import datetime, timedelta
 import twilio.twiml
 # import json
@@ -20,46 +21,46 @@ def handle_message():
     license_plate = str(request.cookies.get('license_plate', ""))
     duration = int(request.cookies.get('duration', 0))
     description = str(request.cookies.get('description'))
+    location = str(request.cookies.get('location', ""))
     body = request.values.get('Body')
-    print("MESSAGE BODY")
     print(body)
-    if step is 0 and "report" in message.lower():
+    if step == 0 and "report" in message.lower():
+        twiml.message("What is your location?")
+    elif step == 1:
+        location = body
         twiml.message("Which Agency Owns the Vehicle? A)SEPTA Bus, B)SEPTA CCT, C)SEPTA, D)PWD, E)PECO, F)Streets, G)Others")  # noqa
-    elif step is 1:
+    elif step == 2:
         twiml.message("What is the License Plate Number? (eg.MG-1234E)")
         agency_name = agency_letter_to_name(body)
-    elif step is 2:
+    elif step == 3:
         twiml.message("What is the Vehicle ID? (eg.105014)")
         license_plate = body
-    elif step is 3:
-        twiml.message("How many minutes has it been Idling for? (eg. 10)")
+    elif step == 4:
+        twiml.message("How many minutes has it been idling for? (eg. 10)")
         vehicle_id = int(body)
-    elif step is 4:
+    elif step == 5:
         twiml.message("Please describe the situation (eg. The driver is sleeping)")  # noqa
         duration = int(body)
-    else:
+    elif step == 6:
         description = body
-        print(vehicle_id)
-        print(agency_name)
-        print(license_plate)
-        print(duration)
-        print(description)
         twiml.message("Thanks!")
-        agency = Agency.query.filter_by(name="SEPTA").first()
+        agency = Agency.query.filter_by(name=agency_name).first()
         new_incident = IncidentReport(
             agency=agency,
             vehicle_id=vehicle_id,
             license_plate=license_plate,
             duration=timedelta(minutes=duration),
             description=description,
+            location=Location(
+                original_user_text=location
+            )
         )
-        print(new_incident)
         db.session.add(new_incident)
         db.session.commit()
-        print 'HERE!'
         step = -1
     step += 1
     response = make_response(str(twiml))
+    # Set cookies
     set_cookie(response, 'messagecount', str(step))
     set_cookie(response, 'agency_name', agency_name)
     set_cookie(response, 'vehicle_id', str(vehicle_id))

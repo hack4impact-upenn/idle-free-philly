82cf86c124b893402a53624fb2206e9ffec99627
Ben Sandler <ben1sandler@gmail.com>
2016-01-12 13:56:27 -0500

    Tmp

--- app/main/messaging.py
+++ app/main/messaging.py
@@ -12,6 +12,10 @@ import twilio.twiml
 @main.route('/report_incident', methods=['GET'])
 def handle_message():
     message = str(request.values.get('Body'))
+    num_media = int(request.values.get('NumMedia'))
+    twilio_hosted_media_url = str(request.values.get('MediaUrl')) \
+        if num_media > 0 else None
+
     twiml = twilio.twiml.Response()
 
     # Retrieve incident cookies
@@ -22,6 +26,7 @@ def handle_message():
     duration = int(request.cookies.get('duration', 0))
     description = str(request.cookies.get('description', ''))
     location = str(request.cookies.get('location', ''))
+    picture_url = str(request.cookies.get('picture_url', ''))
 
     body = str(request.values.get('Body'))
 
@@ -35,60 +40,32 @@ def handle_message():
         duration = 0
         description = ''
         location = ''
+        picture_url = ''
 
-        step = 1
-        twiml.message('What is your location? Be specific! (e.g. "34th and '
-                      'Spruce in Philadelphia PA")')
+        step = handle_start_report(step, twiml)
 
     elif step == 1:
-        location = body
-        step += 1
-        agencies = Agency.query.filter_by(is_official=True).order_by(
-            Agency.name).all()
-        letters = all_strings(len(agencies) + 1)  # one extra letter for Other
-        agencies_listed = '\n'.join(
-            '{}: {}'.format(l, ag.name) for l, ag in zip(letters, agencies)
-        )
-        agencies_listed += '\n{}:Other'.format(letters[-1])
-
-        twiml.message('Which agency owns the vehicle you see idling? Select '
-                      'from the following list or enter {} for Other.'
-                      .format(letters[-1]))
-        twiml.message(agencies_listed)
+        location, step = handle_location_step(body, step, twiml)
 
     elif step == 2:
-        # TODO: handle other
-        agency = agency_letter_to_agency(body)
-        if agency is not None:
-            agency_name = agency.name
-
-        step += 1
-        twiml.message('What is the license plate number? Reply "no" to skip. '
-                      '(e.g. MG1234E)')
+        agency_name, step = handle_agency_step(body, step, twiml)
 
     elif step == 3:
-        if body.lower() != 'no':
-            license_plate = body
-        step += 1
-        twiml.message('What is the Vehicle ID? This is usually on the back or '
-                      'side of the vehicle. (e.g. 105014)')
+        license_plate, step = handle_license_plate_step(body, step, twiml)
 
     elif step == 4:
-        vehicle_id = body
-        step += 1
-        twiml.message('How many minutes have you observed the vehicle idling? '
-                      '(eg. 10)')
+        vehicle_id, step = handle_vehicle_id_step(body, step, twiml)
 
     elif step == 5:
-        duration = int(body)
-        step += 1
-        twiml.message('Please describe the situation (eg. The driver is '
-                      'sleeping)')
-
-    # TODO: mms
+        duration, step = handle_duration_step(body, step, twiml)
 
     elif step == 6:
-        description = body
+        description, step = handle_description_step(body, step, twiml)
+
+    elif step == 7:
+        picture_url, step = handle_picture_step(body, twilio_hosted_media_url,
+                                                step, twiml)
+
         twiml.message('Thanks!')
         (lat, lon) = geocode(location)
         agency = Agency.query.filter_by(name=agency_name).first()
@@ -103,7 +80,8 @@ def handle_message():
                 latitude=lat,
                 longitude=lon,
                 original_user_text=location
-            )
+            ),
+            picture_url=picture_url
         )
         db.session.add(new_incident)
         db.session.commit()
@@ -116,6 +94,7 @@ def handle_message():
         duration = 0
         description = ''
         location = ''
+        picture_url = ''
 
     else:
         twiml.message('Welcome to {}! Please reply "report" to report an '
@@ -134,8 +113,90 @@ def handle_message():
     set_cookie(response, 'duration', str(duration))
     set_cookie(response, 'description', description)
     set_cookie(response, 'location', location)
+    set_cookie(response, 'picture_url', picture_url)
+
     print str(twiml)
     return response
+
+
+def handle_start_report(step, twiml):
+    step = 1
+    twiml.message('What is your location? Be specific! (e.g. "34th and '
+                  'Spruce in Philadelphia PA")')
+    return step
+
+
+def handle_location_step(body, step, twiml):
+    location = body
+    step += 1
+    agencies = Agency.query.filter_by(is_official=True).order_by(
+        Agency.name).all()
+    letters = all_strings(len(agencies) + 1)  # one extra letter for Other
+    agencies_listed = get_agencies_listed(agencies, letters)
+    twiml.message('Which agency owns the vehicle you see idling? Select '
+                  'from the following list or enter {} for Other.'
+                  .format(letters[-1]))
+    twiml.message(agencies_listed)
+    return location, step
+
+
+def handle_agency_step(body, step, twiml):
+    # TODO: handle other
+    agency_name = ''
+    agency = agency_letter_to_agency(body)
+    if agency is not None:
+        agency_name = agency.name
+    step += 1
+    twiml.message('What is the license plate number? Reply "no" to skip. '
+                  '(e.g. MG1234E)')
+    return agency_name, step
+
+
+def handle_license_plate_step(body, step, twiml):
+    license_plate = ''
+    if body.lower() != 'no':
+        license_plate = body
+    step += 1
+    twiml.message('What is the Vehicle ID? This is usually on the back or '
+                  'side of the vehicle. (e.g. 105014)')
+    return license_plate, step
+
+
+def handle_vehicle_id_step(body, step, twiml):
+    vehicle_id = body
+    step += 1
+    twiml.message('How many minutes have you observed the vehicle idling? '
+                  '(eg. 10)')
+    return vehicle_id, step
+
+
+def handle_duration_step(body, step, twiml):
+    duration = int(body)
+    step += 1
+    twiml.message('Please describe the situation (eg. The driver is '
+                  'sleeping)')
+    return duration, step
+
+
+def handle_description_step(body, step, twiml):
+    description = body
+    step += 1
+    twiml.message('Last, can you take a photo of the vehicle and text '
+                  'it back? Reply "no" to skip.')
+    return description, step
+
+
+def handle_picture_step(body, twilio_hosted_media_url, step, twiml):
+    print twilio_hosted_media_url
+    return '', step
+
+
+def get_agencies_listed(agencies, letters):
+    agencies_listed = '\n'.join(
+        '{}: {}'.format(l, ag.name) for l, ag in zip(letters, agencies)
+    )
+    agencies_listed += '\n{}: Other'.format(letters[-1])
+    return agencies_listed
 
 
 def all_strings(max_count):

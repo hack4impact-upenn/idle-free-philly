4f4bd36687a1edc8cc8dc69142fbceba28ed4ec1
Ben Sandler <ben1sandler@gmail.com>
2016-01-13 17:55:37 -0500

    Cleaned up code a bit

--- manage.py
+++ manage.py
@@ -167,7 +167,7 @@ def run_scheduler():
     for _ in xrange(10):
         try:
             scheduler.run()
-        except ValueError, exc:
+        except ValueError as exc:
             if exc.message == 'There\'s already an active RQ scheduler':
                 scheduler.log.info(
                     'An RQ scheduler instance is already running. Retrying in '
@@ -175,7 +175,7 @@ def run_scheduler():
                 )
                 time.sleep(10)
             else:
-                raise
+                raise exc
 
 if __name__ == '__main__':
     manager.run()

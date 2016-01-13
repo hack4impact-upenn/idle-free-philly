f9bd953c1f36a1d6297bbad3cbede75991a44762
Ben Sandler <ben1sandler@gmail.com>
2016-01-13 03:26:55 -0500

    Finished up adding error reporting to twilio integration

--- app/main/messaging.py
+++ app/main/messaging.py
@@ -14,11 +14,7 @@ from twilio.rest import TwilioRestClient
 
 @main.route('/report_incident', methods=['GET'])
 def handle_message():
-    body = str(request.values.get('Body'))
-    num_media = int(request.values.get('NumMedia'))
-    twilio_hosted_media_url = str(request.values.get('MediaUrl0')) \
-        if num_media > 0 else None
-    message_sid = str(request.values.get('MessageSid'))
+    body, message_sid, twilio_hosted_media_url = get_request_values()
 
     twiml = twilio.twiml.Response()
 
@@ -32,8 +28,6 @@ def handle_message():
     location = str(request.cookies.get('location', ''))
     picture_url = str(request.cookies.get('picture_url', ''))
 
-    print locals()  # TODO: for debugging
-
     if 'report' == body.lower():
         # reset report variables/cookies
         vehicle_id = ''
@@ -45,7 +39,6 @@ def handle_message():
         picture_url = ''
 
         step = handle_start_report(step, twiml)
-        step = 1
 
     elif step == 1:
         location, step = handle_location_step(body, step, twiml)
@@ -54,56 +47,32 @@ def handle_message():
         agency_name, step = handle_agency_step(body, step, twiml)
 
     elif step == 3:
-        license_plate, step = handle_license_plate_step(body, step, twiml)
+        agency_name, step = handle_other_agency_step(body, step, twiml)
 
     elif step == 4:
-        vehicle_id, step = handle_vehicle_id_step(body, step, twiml)
+        license_plate, step = handle_license_plate_step(body, step, twiml)
 
     elif step == 5:
-        duration, step = handle_duration_step(body, step, twiml)
+        vehicle_id, step = handle_vehicle_id_step(body, step, twiml)
 
     elif step == 6:
-        description, step = handle_description_step(body, step, twiml)
+        duration, step = handle_duration_step(body, step, twiml)
 
     elif step == 7:
+        description, step = handle_description_step(body, step, twiml)
+
+    elif step == 8:
         picture_url, step, image_job_id = handle_picture_step(
             body, step, message_sid, twilio_hosted_media_url, twiml)
 
-        (lat, lon) = geocode(location)
-        agency = Agency.query.filter_by(name=agency_name).first()
-        # TODO: handle empty strings, set attributes to None
-        new_incident = IncidentReport(
-            agency=agency,
-            vehicle_id=vehicle_id,
-            license_plate=license_plate if license_plate else None,
-            duration=timedelta(minutes=duration),
-            description=description,
-            location=Location(
-                latitude=lat,
-                longitude=lon,
-                original_user_text=location
-            ),
-            picture_url=picture_url if picture_url else None
-        )
-        db.session.add(new_incident)
-        db.session.commit()
+        new_incident = handle_create_report(agency_name, description, duration,
+                                            license_plate, location,
+                                            picture_url, vehicle_id)
 
-        get_queue().enqueue(
-            attach_image_to_incident_report,
-            depends_on=image_job_id,
-            incident_report=new_incident,
-            image_job_id=image_job_id,
-        )
+        start_attach_job(image_job_id, new_incident)
 
         # reset report variables/cookies
         step = 0
-        vehicle_id = ''
-        agency_name = ''
-        license_plate = ''
-        duration = 0
-        description = ''
-        location = ''
-        picture_url = ''
 
     else:
         twiml.message('Welcome to {}! Please reply "report" to report an '
@@ -121,8 +90,53 @@ def handle_message():
     set_cookie(response, 'location', location)
     set_cookie(response, 'picture_url', picture_url)
 
-    print str(twiml)
     return response
+
+
+def start_attach_job(image_job_id, new_incident):
+    if image_job_id is not None:
+        get_queue().enqueue(
+            attach_image_to_incident_report,
+            depends_on=image_job_id,
+            incident_report=new_incident,
+            image_job_id=image_job_id,
+        )
+
+
+def handle_create_report(agency_name, description, duration, license_plate,
+                         location, picture_url, vehicle_id):
+    lat, lon = geocode(location)
+    agency = Agency.get_agency_by_name(agency_name)
+    if agency is None:
+        agency = Agency(name=agency_name, is_official=False,
+                        is_public=True)
+        db.session.add(agency)
+        db.session.commit()
+    new_incident = IncidentReport(
+        agency=agency,
+        vehicle_id=vehicle_id,
+        license_plate=license_plate if license_plate else None,
+        duration=timedelta(minutes=duration),
+        description=description,
+        location=Location(
+            latitude=lat,
+            longitude=lon,
+            original_user_text=location
+        ),
+        picture_url=picture_url if picture_url else None
+    )
+    db.session.add(new_incident)
+    db.session.commit()
+    return new_incident
+
+
+def get_request_values():
+    body = str(request.values.get('Body')).lower().strip()
+    num_media = int(request.values.get('NumMedia'))
+    twilio_hosted_media_url = str(request.values.get('MediaUrl0')) \
+        if num_media > 0 else None
+    message_sid = str(request.values.get('MessageSid'))
+    return body, message_sid, twilio_hosted_media_url
 
 
 def handle_start_report(step, twiml):
@@ -134,9 +148,9 @@ def handle_start_report(step, twiml):
 
 def handle_location_step(body, step, twiml):
     validator_form = IncidentReportForm()
-
     errors = data_errors(form=validator_form, field=validator_form.location,
                          data=body)
+
     if geocode(body) == (None, None):
         errors.append('We could not find that location. Please respond with a '
                       'full address including city and state.')
@@ -147,88 +161,159 @@ def handle_location_step(body, step, twiml):
             Agency.name).all()
         letters = all_strings(len(agencies) + 1)  # one extra letter for Other
         agencies_listed = get_agencies_listed(agencies, letters)
-        twiml.message('Which agency owns the vehicle you see idling? Select '
-                      'from the following list or enter {} for Other.'
+        twiml.message('Which agency operates the vehicle you see idling? '
+                      'Select from the following list or enter {} for Other.'
                       .format(letters[-1]))
         twiml.message(agencies_listed)
     else:
         location = ''
-        twiml.message('Sorry, there were some errors with your response. '
-                      'Please enter the location again.')
-        twiml.message('Errors:\n{}'.format('\n'.join(errors)))
+        reply_with_errors(errors, twiml, 'location')
+
     return location, step
 
 
-def handle_agency_step(body, step, twiml):
-    # TODO: handle other
-    agency_name = ''
-    agency = agency_letter_to_agency(body)
-    if agency is not None:
-        agency_name = agency.name
+def handle_agency_step(body_upper, step, twiml):
+    body_upper = body_upper.upper()
+    agencies = Agency.query.filter_by(is_official=True).all()
+    letters = all_strings(len(agencies) + 1)  # one extra letter for Other
+    letters_to_agency = dict(zip(letters, agencies))
+
+    if body_upper == letters[-1]:  # Other
+        step += 1
+        agency_name = ''
+        twiml.message('You selected Other. What is the name of the agency '
+                      'which operates the vehicle?')
+
+    elif body_upper in letters_to_agency.keys():
+        step += 2
+        agency_name = letters_to_agency[body_upper].name
+        twiml.message('What is the license plate number? Reply "no" to skip. '
+                      '(e.g. MG1234E)')
+
+    else:
+        agency_name = ''
+        twiml.message('Please enter a letter A through {}.'
+                      .format(letters[-1]))
+
+    return agency_name, step
+
+
+def handle_other_agency_step(body, step, twiml):
+    agency_name = body
     step += 1
     twiml.message('What is the license plate number? Reply "no" to skip. '
                   '(e.g. MG1234E)')
+
     return agency_name, step
 
 
 def handle_license_plate_step(body, step, twiml):
-    license_plate = ''
-    if body.lower() != 'no':
+    if body.lower() == 'no':
+        body = ''
+
+    validator_form = IncidentReportForm()
+    errors = data_errors(
+        form=validator_form,
+        field=validator_form.license_plate,
+        data=body
+    )
+    if len(errors) == 0:
         license_plate = body
-    step += 1
-    twiml.message('What is the Vehicle ID? This is usually on the back or '
-                  'side of the vehicle. (e.g. 105014)')
+        step += 1
+        twiml.message('What is the Vehicle ID? This is usually on the back or '
+                      'side of the vehicle. (e.g. 105014)')
+    else:
+        license_plate = ''
+        reply_with_errors(errors, twiml, 'license plate')
+
     return license_plate, step
 
 
 def handle_vehicle_id_step(body, step, twiml):
-    vehicle_id = body
-    step += 1
-    twiml.message('How many minutes have you observed the vehicle idling? '
-                  '(eg. 10)')
+    validator_form = IncidentReportForm()
+    errors = data_errors(form=validator_form, field=validator_form.vehicle_id,
+                         data=body)
+
+    if len(errors) == 0:
+        vehicle_id = body
+        step += 1
+        twiml.message('How many minutes have you observed the vehicle idling? '
+                      '(eg. 10)')
+    else:
+        vehicle_id = ''
+        reply_with_errors(errors, twiml, 'vehicle ID')
+
     return vehicle_id, step
 
 
 def handle_duration_step(body, step, twiml):
-    duration = int(body)
-    step += 1
-    twiml.message('Please describe the situation (eg. The driver is '
-                  'sleeping)')
+    errors = []
+    try:
+        body = int(body)
+    except ValueError:
+        errors.append('Please enter a valid integer.')
+
+    if len(errors) == 0:
+        duration = body
+        step += 1
+        twiml.message('Please describe the situation (eg. The driver is '
+                      'sleeping)')
+    else:
+        duration = 0
+        reply_with_errors(errors, twiml, 'duration')
+
     return duration, step
 
 
 def handle_description_step(body, step, twiml):
-    description = body
-    step += 1
-    twiml.message('Last, can you take a photo of the vehicle and text '
-                  'it back? Reply "no" to skip.')
+    validator_form = IncidentReportForm()
+    errors = data_errors(form=validator_form, field=validator_form.description,
+                         data=body)
+
+    if len(errors) == 0:
+        description = body
+        step += 1
+        twiml.message('Last, can you take a photo of the vehicle and text '
+                      'it back? Reply "no" to skip.')
+    else:
+        description = ''
+        reply_with_errors(errors, twiml, 'description')
+
     return description, step
 
 
 def handle_picture_step(body, step, message_sid, twilio_hosted_media_url,
                         twiml):
-    account_sid = current_app.config['TWILIO_ACCOUNT_SID']
-    auth_token = current_app.config['TWILIO_AUTH_TOKEN']
+    image_job_id = None
+    if twilio_hosted_media_url is not None:
+        account_sid = current_app.config['TWILIO_ACCOUNT_SID']
+        auth_token = current_app.config['TWILIO_AUTH_TOKEN']
 
-    image_job_id = get_queue().enqueue(
-        upload_image,
-        imgur_client_id=current_app.config['IMGUR_CLIENT_ID'],
-        imgur_client_secret=current_app.config['IMGUR_CLIENT_SECRET'],
-        app_name=current_app.config['APP_NAME'],
-        image_url=twilio_hosted_media_url
-    ).id
+        image_job_id = get_queue().enqueue(
+            upload_image,
+            imgur_client_id=current_app.config['IMGUR_CLIENT_ID'],
+            imgur_client_secret=current_app.config['IMGUR_CLIENT_SECRET'],
+            app_name=current_app.config['APP_NAME'],
+            image_url=twilio_hosted_media_url
+        ).id
 
-    get_rq_scheduler().enqueue_in(
-        timedelta(minutes=10),
-        delete_mms,
-        account_sid=account_sid,
-        auth_token=auth_token,
-        message_sid=message_sid
-    )
+        get_rq_scheduler().enqueue_in(
+            timedelta(minutes=10),
+            delete_mms,
+            account_sid=account_sid,
+            auth_token=auth_token,
+            message_sid=message_sid
+        )
     twiml.message('Thanks! See your report at {}'
                   .format(url_for('main.index')))
 
     return '', step, image_job_id
+
+
+def reply_with_errors(errors, twiml, field_name):
+    twiml.message('Sorry, there were some errors with your response. '
+                  'Please enter the {} again.'.format(field_name))
+    twiml.message('Errors:\n{}'.format('\n'.join(errors)))
 
 
 def delete_mms(account_sid, auth_token, message_sid):
@@ -283,14 +368,6 @@ def set_cookie(resp, key, val):
     expires = datetime.utcnow() + timedelta(hours=1)
     expires_str = expires.strftime('%a, %d %b %Y %H:%M:%S GMT')
     resp.set_cookie(key, value=val, expires=expires_str)
-
-
-def agency_letter_to_agency(input_letter):
-    agencies = Agency.query.filter_by(is_official=True).all()
-    letters = all_strings(len(agencies) + 1)  # one extra letter for Other
-    letters_to_agency = dict(zip(letters, agencies))
-    letters_to_agency[letters[-1]] = None
-    return letters_to_agency[input_letter]
 
 
 def data_errors(field, data, form):

01a5317e5b85503b114e98caf1f82a76c57ce948
Ben Sandler <ben1sandler@gmail.com>
2016-03-31 13:04:55 -0400

    Got submit form working

--- app/main/views.py
+++ app/main/views.py
@@ -1,18 +1,18 @@
 from flask import render_template
 from . import main
-from app import models, db, reports
+from app import models, db
+from app.reports.forms import IncidentReportForm
 from app.models import IncidentReport, Agency, EditableHTML
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 
 @main.route('/', methods=['GET', 'POST'])
 @main.route('/map', methods=['GET', 'POST'])
 def index():
-    form = reports.forms.IncidentReportForm()
+    form = IncidentReportForm()
     agencies = Agency.query.all()
 
     if form.validate_on_submit():
-        print "Submit"
         l = models.Location(original_user_text=form.location.data,
                             latitude=form.latitude.data,
                             longitude=form.longitude.data)
@@ -21,15 +21,19 @@ def index():
             vehicle_id=form.vehicle_id.data,
             license_plate=form.license_plate.data,
             location=l,
-            date=form.date.data,
-            time=form.time.data,
+            date=datetime.combine(form.date.data, form.time.data),
             duration=timedelta(minutes=form.duration.data),
             agency=form.agency.data,
             description=form.description.data,
         )
-        print(new_incident)
         db.session.add(new_incident)
         db.session.commit()
+
+    # pre-populate form
+    form.date.default = datetime.now()
+    form.time.default = datetime.now()
+    form.process()
+
     return render_template('main/map.html',
                            agencies=agencies,
                            form=form,
